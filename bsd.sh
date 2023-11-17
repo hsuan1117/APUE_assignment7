e7d9c578bf7ca9bab91b372cfa70587121ccbf00
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2023-11-17 11:02:29 +0800

    update code

--- bsd.sh
+++ bsd.sh
@@ -1,10 +1,10 @@
 # One FreeBSD 13.2-RELEASE-p4 FreeBSD 13.2-RELEASE-p4 GENERIC amd64
 ryanchang1117@freebsd-13-1:~/APUE_assignment7 $ ./assignment7
-
-child process pid: 1044, pgid: 1044, sid: 1044
+parent process pid: 1256, pgid: 1256, sid: 903
+child process pid: 1257, pgid: 1257, sid: 1257
  PID PGID TPGID
  902  900     0
-1044 1044     0
-1045 1044     0
- 903  903  1043
-1043 1043  1043
+1257 1257     0
+1258 1257     0
+ 903  903  1256
+1256 1256  1256
